797ae0f3b9ddefa7bd5a4f84d80a1c97d478b038
dungem <letrongdung060703@gmail.com>
2023-09-19 13:47:47 +0700

    abc

--- src/main/java/com/example/demo5/HelloServlet.java
+++ src/main/java/com/example/demo5/HelloServlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet") //trongdung060703
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
